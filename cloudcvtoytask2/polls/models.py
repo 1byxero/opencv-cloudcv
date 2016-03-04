c9aebdf3ca77c58192f6a95115a5faa9e2e0f2c5
Anuj Godase <godaseanuj@gmail.com>
2016-03-04 13:26:47 +0530

    opencvmodules working

--- cloudcvtoytask2/polls/models.py
+++ cloudcvtoytask2/polls/models.py
@@ -3,6 +3,10 @@ from __future__ import unicode_literals
 from django.db import models
 
 
-class Document(models.Model):
-    docfile = models.FileField(upload_to='documents/%Y/%m/%d')
+class Document(models.Model):	
+    docfile = models.FileField(upload_to='documents')
+
+    def __unicode__(self):
+		return str(self.docfile)
+
 # Create your models here.
--- cloudcvtoytask2/polls/views.py
+++ cloudcvtoytask2/polls/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render,redirect
 from forms import DocumentForm
+from models import Document
+
+
+
 
 # Create your views here.
 
@@ -9,6 +13,11 @@ def index(request):
 		form = DocumentForm(request.POST, request.FILES)
 		if form.is_valid():
 			newdoc = Document(docfile = request.FILES['docfile'])
+			newdoc.save()
+			return redirect('/', name="index")			
+		else:
+			return HttpResponse("Error brah")		
 	else:
 		form = DocumentForm()
-		return render(request,'polls/index.html',{"form":form})
+		documents = Document.objects.all()				
+		return render(request,'polls/index.html',{"form":form,"documents":documents})
